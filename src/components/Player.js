d057f19038e9ab56bf501e4a382d0dbf91f0405a
Lloyd Chapple <lloydchaps@gmail.com>
2021-05-27 16:55:46 +0100

    add player

---
+++ src/components/Player.js
@@ -0,0 +1,13 @@
+function Player() {
+    return (
+        <div className="c-player">
+            <audio></audio>
+            <h4>Playing now</h4>
+            {/* DETAILS */}
+            {/* CONTROLS */}
+            <p><strong>Next up:</strong> Kindest Regards by Witt Lowry</p>
+        </div>
+    )
+}
+
+export default Player
