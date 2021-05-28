cbe9ace49f60870e468b17b473997fa7eea64d17
Lloyd Chapple <lloydchaps@gmail.com>
2021-05-28 15:19:06 +0100

    made it look like a weird ipod

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,7 +5,6 @@ function Player(props) {
     return (
         <div className="c-player">
             <audio></audio>
-            <h4>Playing now</h4>
             <PlayerDetails song={props.song} />
             <PlayerControls />
             <p><strong>Next up:</strong> {props.nextSong.title} by {props.nextSong.artist} </p>
